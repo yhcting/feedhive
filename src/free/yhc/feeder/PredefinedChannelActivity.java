4521533539996ab2beb9ddc9c983a8dd4fad6044
Younghyung Cho <yhcting77@gmail.com>
2012-06-13 09:16:05 +0900

    Predefined channel is enriched(to ver 3) and verion is updated to 1.3.8.

--- src/free/yhc/feeder/PredefinedChannelActivity.java
+++ src/free/yhc/feeder/PredefinedChannelActivity.java
@@ -63,7 +63,7 @@ UnexpectedExceptionHandler.TrackedModule {
     // These values SHOULD MATCH asset DB.
     //
     // ========================================================================
-    private static final int DB_VERSION     = 2;
+    private static final int DB_VERSION     = 3;
 
     private static final String DB_NAME     = "predefined_channels.db";
     private static final String DB_ASSET    = "channels.db";
