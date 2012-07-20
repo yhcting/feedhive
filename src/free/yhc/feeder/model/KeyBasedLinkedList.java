29f7647d12f5807a7188d676b6164a80f52391c9
Younghyung Cho <yhcting77@gmail.com>
2012-07-20 16:26:44 +0900

    File header comments for license are added to recently created files.

--- src/free/yhc/feeder/model/KeyBasedLinkedList.java
+++ src/free/yhc/feeder/model/KeyBasedLinkedList.java
@@ -1,3 +1,23 @@
+/*****************************************************************************
+ *    Copyright (C) 2012 Younghyung Cho. <yhcting77@gmail.com>
+ *
+ *    This file is part of Feeder.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License
+ *    (<http://www.gnu.org/licenses/lgpl.html>) for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
 package free.yhc.feeder.model;
 
 import java.util.Iterator;
@@ -85,6 +105,7 @@ public class KeyBasedLinkedList<T> {
 
     public T[]
     toArray(T[] a) {
+        // NOT TESTED enough yet!
         Elem[] es = l.toArray(new Elem[0]);
         if (a.length < es.length)
             a = (T[])Utils.newArray(a.getClass().getComponentType(), es.length);
